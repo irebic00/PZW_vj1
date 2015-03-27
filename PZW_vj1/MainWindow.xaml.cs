eafef9309ff9f7f3cd070fd146f5d1baa6577127
irebic00 <irebic00@fesb.hr>
2015-03-27 13:03:40 +0100

    First Commit

---
+++ PZW_vj1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PZW_vj1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.addLeft.Click += new RoutedEventHandler(addLeft_Click);
+            this.addRight.Click += new RoutedEventHandler(addRight_Click);
+        }
+
+        private void addLeft_Click(object sender, RoutedEventArgs e)
+        {
+            var tmp = new Rectangle();
+
+            tmp.Fill = temporaryRectangle.Fill;
+            tmp.Width = 40;
+            tmp.Height = 40;
+            tmp.Margin = new Thickness(5);
+            leftPanel.Children.Add(tmp);
+        }
+
+        private void addRight_Click(object sender, RoutedEventArgs e)
+        {
+            var tmp = new Rectangle();
+            var blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+
+            tmp.Fill = temporaryRectangle.Fill;
+            tmp.Width = 350;
+            tmp.Height = 30;
+            tmp.Stroke = blackBrush;
+            tmp.Margin = new Thickness(5);
+            rightPanel.Children.Add(tmp);
+        }
+    }
+}
---
+++ PZW_vj1/UniversalValueConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace WPFUniversalConveter
+{
+    public class UniversalValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // obtain the conveter for the target type
+            TypeConverter converter = TypeDescriptor.GetConverter(targetType);
+
+            try
+            {
+                // determine if the supplied value is of a suitable type
+                if (converter.CanConvertFrom(value.GetType()))
+                {
+                    // return the converted value
+                    return converter.ConvertFrom(value);
+                }
+                else
+                {
+                    // try to convert from the string representation
+                    return converter.ConvertFrom(value.ToString());
+                }
+            }
+            catch (Exception)
+            {
+                return value;
+            }
+         
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
